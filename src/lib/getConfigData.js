2a564ef2341a006c168d0edb0e8b3043248665f7
AIZENBERG Yeonathan <yeonathan.aizenberg@travelfactory.fr>
2022-07-01 23:02:23 +0300

    save done

--- src/lib/getConfigData.js
+++ src/lib/getConfigData.js
@@ -4,12 +4,10 @@ const configLambdaURL = "https://vxuprbsteidguxpsvpg4xizfhy0ssedc.lambda-url.eu-
 
 const getConfigData = async station => {
     const response = await axios.get(configLambdaURL, { params: { key: `${station}` } })
-    const formatedResponse = JSON.parse(response.data.Item.data).pageData
-    // When do the call from postman, the data format will be odd, use this commeted code to be able to click on the main save btn and have tge data on hte DB on the correct format. FIX THAT iSSUE!!!!!!
-    // console.log("formatedResponse",formatedResponse)
-    // console.log("formatedResponse.data",(JSON.parse(formatedResponse).pageData))
-    // return JSON.parse(formatedResponse).pageData
-    return formatedResponse
+    const formatedResponse = response.data.Item.data
+    const configData = JSON.parse(formatedResponse).pageData
+
+    return JSON.parse(configData).pageData
 }
 
 export default getConfigData
